be0563bb1f91e54ba7dd11da3a4f856ceb08f830
Stefano Debenedetti <44903077+demaledetti@users.noreply.github.com>
2025-06-09 16:03:15 +0200

    Document the freeze/pin strategy

--- pin.sh
+++ pin.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+# for discussion and documentation of full freeze file vs freezing only index-state:
+# see https://github.com/haskell/cabal/issues/8059#issuecomment-1076024522
+# and https://github.com/haskell/cabal/pull/9984/files
+
 set -e
 
 rm -f cabal.project.freeze
